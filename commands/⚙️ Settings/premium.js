5228d19c584ad6192ca6d68e9013df3c2216e415
Raichu <43868820+robs212@users.noreply.github.com>
2023-10-01 10:25:40 +0700

    Update premium.js

--- commands/⚙️ Settings/premium.js
+++ commands/⚙️ Settings/premium.js
@@ -51,12 +51,3 @@ module.exports = {
     return message.reply(`✅ **APPLIED FOR PREMIUM**`)
   }
 };
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://discord.gg/milrato
- * @INFO
- * Work for Milrato Development | https://milrato.dev
- * @INFO
- * Please mention him / Milrato Development, when using this Code!
- * @INFO
- */
